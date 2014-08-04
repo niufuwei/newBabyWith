9a8e877619230e537cad945d4542824aa676e376
XXF <feng.zhi.shang@qq.com>
2014-08-04 14:21:38 +0800

    werwerw
    weqwerqwerwerwer

--- BabyWith/BabyWith/MainAppDelegate.m
+++ BabyWith/BabyWith/MainAppDelegate.m
@@ -62,8 +62,8 @@
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     self.window.backgroundColor  = babywith_background_color;
     
-    
-    
+//    //状态栏 白色
+//    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     //添加本地计时器用来几时验证码
 
     [[NSUserDefaults standardUserDefaults] setObject:@"0" forKey:@"sendCheckMessage"];
